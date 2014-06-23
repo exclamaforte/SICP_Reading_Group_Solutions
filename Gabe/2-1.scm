6abb8bfb5ce13476c74ecfc91a12c93b38b18ff4
exclamaforte <exclamaforte@gmail.com>
2014-06-23 11:07:42 -0500

    did 2.7 through 2.12

--- Gabe/2-1.scm
+++ Gabe/2-1.scm
@@ -104,3 +104,65 @@
   (iter n 0))
 
 ;2-6 seems hard
+
+;2-7
+(define (make-interval l u)
+  (cons l u))
+(define (lower-bound f)
+  (car f))
+(define (upper-bound f)
+  (cdr f))
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+;2-10
+(define (div-interval x y)
+  (if (= (- (upper-bound y) (lower-bound y)) 0)
+      0
+      (mul-interval x 
+                    (make-interval (/ 1.0 (upper-bound y))
+                                   (/ 1.0 (lower-bound y))))))
+
+;2-8
+(define (sub-interval x y)
+  (add-interval x (make-interval (- 0 (lower-bound y)) (- 0 (upper-bound y)))))
+
+;2-9
+;multiply (-1, 1) and (-1, 1) vs multiply (-1, 1) and (5, 7), same with divide
+
+;2-11 cases are, for each interval, lower and upper > 0, < 0, lower <= 0 and upper >= 0 ^2 = 9 choices
+(define (mul-interval x y)
+  (cond ((and (< (lower-bound x) 0) (< (upper-bound x) 0)))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+;2-12
+(define (make-center-percent center percentage)
+  (make-center-width center (* center (/ percentage 100))))
+
+(define (percent interv)
+  (/ (width interv) (center interv)))
+
+(define (par1 r1 r2)
+  (div-interval (mul-interval r1 r2)
+                (add-interval r1 r2)))
+
+(define (par2 r1 r2)
+  (let ((one (make-interval 1 1)))
+    (div-interval one
+                  (add-interval (div-interval one r1)
+                                (div-interval one r2)))))
